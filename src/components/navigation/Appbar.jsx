49316f6ce97b1bce91bac9510b61177b6cb887df
Gagandeep Raju <raju.gagandeep@gmail.com>
2022-06-06 23:23:03 -0400

    table hover over nav tabs

--- src/components/navigation/Appbar.jsx
+++ src/components/navigation/Appbar.jsx
@@ -1,7 +1,9 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import Popover from "@mui/material/Popover";
+import ModalLink from "./ModalMenu";
 
 const tabs = [
   "SALE",
@@ -16,19 +18,51 @@ const tabs = [
 ];
 
 export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [modalName, setModalName] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+    setModalName(event.currentTarget.innerText);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const open = Boolean(anchorEl);
   return (
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
       <Tabs value={value} onChange={handleChange} centered>
         {tabs.map((tab) => {
-          return <Tab key={tab} label={tab} />;
+          return <Tab key={tab} label={tab} onMouseEnter={handlePopoverOpen} />;
         })}
       </Tabs>
+      <Popover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: "none",
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <ModalLink name={modalName} />
+      </Popover>
     </Box>
   );
 }
